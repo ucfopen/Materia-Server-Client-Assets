5c17bc7a99c4dec1d9533a4cbcfef23a67ae5d6a
Ian Turgeon <iturgeon@gmail.com>
2018-02-17 15:06:50 -0500

    decaffeinate: Run post-processing cleanups on ctrl-admin-user.coffee and 61 other files

--- src/js/filters/filter-escape.js
+++ src/js/filters/filter-escape.js
@@ -1,2 +1,4 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 const app = angular.module('materia');
 app.filter('escape', () => window.encodeURIComponent);
