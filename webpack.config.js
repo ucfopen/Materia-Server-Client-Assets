e25ec2e00c9cb6a4f49a994d271119ba67bdaee6
Ian Turgeon <iturgeon@gmail.com>
2018-02-19 12:24:46 -0500

    bug fix to work when included in materia-server

--- webpack.config.js
+++ webpack.config.js
@@ -142,7 +142,6 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
