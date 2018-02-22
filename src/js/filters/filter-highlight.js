ba377d43ef37810c28cd0f511cf5f7ea9445bd81
Ian Turgeon <iturgeon@gmail.com>
2018-02-22 12:38:08 -0500

    more tests coverage

--- src/js/filters/filter-highlight.js
+++ src/js/filters/filter-highlight.js
@@ -14,18 +14,25 @@ const escapeHtml = string => String(string).replace(/[&<>"'\/]/g, s => entityMap
 // Highlights search matches, used on My Widgets sidebar
 app.filter('highlight', function($sce) {
 	return (text, search) => {
+		// escape special characters from the source text
 		text = escapeHtml(text)
 		if (search) {
 			const searchTerms = search.split(' ')
-			for (search of searchTerms) {
-				text = text.replace(new RegExp(`(${search})`, 'gi'), (a, b, c, d) => {
-					const t = d.substr(c).split('<')
+			searchTerms.forEach(term => {
+				// find term in text and wrap it with a span
+				text = text.replace(new RegExp(`${term}`, 'gi'), (match, offset) => {
+					// @TODO: no comments left for this by previous dev
+					// not sure it's purpose
+					// sort of looks like it's trying to prevent accidentally
+					// nesting spans or overlapping html tags?
+					// I haven't been able to get it to enter this condition
+					const t = text.substr(offset).split('<')
 					if (t[0].indexOf('>') !== -1) {
-						return a
+						return term
 					}
-					return `<span class="highlighted">${a}</span>`
+					return `<span class="highlighted">${term}</span>`
 				})
-			}
+			})
 		}
 		return $sce.trustAsHtml(text)
 	}
