f76c3836e71219051ffebabe16c66ef73f40d7ac
Nathan Dabu <nathan.dabu@ucf.edu>
2019-02-21 10:19:22 -0500

    on catalog page: reorganize filters on mobile, add 'x widgets hidden by filters', styling fixes

--- src/js/controllers/ctrl-widget-catalog.js
+++ src/js/controllers/ctrl-widget-catalog.js
@@ -1,25 +1,44 @@
 const app = angular.module('materia')
 app.controller('widgetCatalogCtrl', function(Please, $scope, $window, widgetSrv) {
-	$scope.displayAll = false // TODO not used
+	$scope.displayAll = false
 	$scope.widgets = []
 	$scope.query = ""
-	$scope.isMini = true
 	$scope.count = -1
 	$scope.filters = {
-		scorable: false,
-		mobile: false,
-		media: false,
-		qa: false,
-		mc: false
+		'Scorable': {
+			active: false,
+			text: "Collects Scores",
+			type: 1
+		},
+		'Mobile Friendly': {
+			active: false,
+			text: "Mobile Friendly",
+			type: 1
+		},
+		'Media': {
+			active: false,
+			text: "Uploadable Media",
+			type: 1
+		},
+		'Question/Answer': {
+			active: false,
+			text: "Question/Answer",
+			type: 2
+		},
+		'Multiple Choice': {
+			active: false,
+			text: "Multiple Choice",
+			type: 2
+		}
+	}
+	$scope.displayAll = false
+	$scope.ready = false
+	$scope.toggleDisplayAll = () => $scope.displayAll = !$scope.displayAll
+	$scope.toggleFeature = feature => {
+		$scope.filters[feature].active = !$scope.filters[feature].active
+		_createGrid()
 	}
 
-	const featureKeys = {
-		scorable: 'Scorable',
-		mobile: 'Mobile Friendly',
-		qa: 'Question/Answer',
-		mc: 'Multiple Choice',
-		media: 'Media'
-	}
 
 	// checks filters and returns true if the widget should be shown, false if filtered out
 	const _showWidget = widget => {
@@ -27,12 +46,15 @@ app.controller('widgetCatalogCtrl', function(Please, $scope, $window, widgetSrv)
 		const wSupport = widget.meta_data.supported_data
 
 		for (let filterName in $scope.filters) {
-			const filterOn = $scope.filters[filterName]
-			const metaValue = featureKeys[filterName]
+			const filterOn = $scope.filters[filterName].active
 
-			if (filterOn && !wFeatures.includes(metaValue) && !wSupport.includes(metaValue)) {
+			if (filterOn && !wFeatures.includes(filterName) && !wSupport.includes(filterName)) {
 				return false
 			}
+		}
+
+		if (!$scope.displayAll && widget.in_catalog == "0") {
+			return false
 		}
 
 		if ($scope.query.length) {
@@ -53,6 +75,8 @@ app.controller('widgetCatalogCtrl', function(Please, $scope, $window, widgetSrv)
 				widget.visible = false
 			}
 		}
+
+		$scope.mobileFiltersOpen = false
 		Please.$apply()
 	}
 
@@ -61,21 +85,20 @@ app.controller('widgetCatalogCtrl', function(Please, $scope, $window, widgetSrv)
 		if (!widgets || !widgets.length || !widgets.length > 0) {
 			return
 		}
-		// setup some default values
+		// set up some default values
 		widgets.forEach(widget => {
 			widget.icon = Materia.Image.iconUrl(widget.dir, 275)
-			widget.visible = true
 		})
+
 		$scope.widgets = widgets
 
-		// draw the grid once widgets are initially sized (need height)
 		_createGrid()
+		$scope.ready = true // prevents animation when loading
 		$scope.$watch('query', _createGrid)
-		$scope.$watchCollection('filters', _createGrid)
+		$scope.$watch('displayAll', _createGrid)
 	})
 
-	// TODO this doesn't do anything
-	// DISPLAY_TYPE can be rendered in the page by the server
+	// DISPLAY_TYPE can be rendered in the page by the server (on widgets/all)
 	if (typeof DISPLAY_TYPE !== 'undefined' && DISPLAY_TYPE === 'all') {
 		$scope.displayAll = true
 	}
