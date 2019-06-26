b86ab79384f8516fa9df31912ff228b9add9a6ef
Weining Li <weining.li@ucf.edu>
2019-06-26 09:48:48 -0400

    added test for submit participation function

--- src/js/materia/materia.score.test.js
+++ src/js/materia/materia.score.test.js
@@ -13,6 +13,7 @@ describe('Materia.Score', () => {
 		expect(Score.submitInteractionForScoring).toBeDefined()
 		expect(Score.submitFinalScoreFromClient).toBeDefined()
 		expect(Score.submitQuestionForScoring).toBeDefined()
+		expect(Score.submitUnscorableForParticipation).toBeDefined()
 		expect(Score.addGlobalScoreFeedback).toBeDefined()
 		expect(Score.addScoreData).toBeDefined()
 	})
@@ -25,6 +26,11 @@ describe('Materia.Score', () => {
 	it('addGlobalScoreFeedback calls addLog', () => {
 		Score.addGlobalScoreFeedback('my message')
 		expect(mockAddLog).toHaveBeenLastCalledWith(1008, '0', 'my message')
+	})
+
+	it('submitUnscorableForParticipation calls addLog', () => {
+		Score.submitUnscorableForParticipation(01000101)
+		expect(mockAddLog).toHaveBeenLastCalledWith(1006, 01000101, 'Participation', 100)
 	})
 
 	it('submitQuestionForScoring calls addLog', () => {
