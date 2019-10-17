18ac3b2436921ed86ec2be07300470ba45b55f44
Weining Li <weining.li@ucf.edu>
2019-10-17 17:08:58 -0400

    assign correct access level when canceling a demote

--- src/js/controllers/ctrl-collaboration.js
+++ src/js/controllers/ctrl-collaboration.js
@@ -253,9 +253,10 @@ app.controller('CollaborationController', function(
 	}
 
 	const _cancelDemote = user => {
+		const accessLevel = String($scope.selected.accessLevel)
 		user.warning = false
 		user.remove = false
-		return (user.access = ACCESS.FULL)
+		return (user.access = accessLevel)
 	}
 
 	$scope.inputs = { userSearchInput: '' }
