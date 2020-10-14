e90dd5676da91185867a4f6d4b92c3352160cbc2
Corey Peterson <corey.peterson@ucf.edu>
2020-10-14 16:30:00 -0400

    Renamed ctrl-create test to ctrl-widget-creator test

--- src/js/controllers/ctrl-widget-creator.test.js
+++ src/js/controllers/ctrl-widget-creator.test.js
@@ -1,4 +1,4 @@
-describe('createCtrl', () => {
+describe('WidgetCreatorCtrl', () => {
 	var $controller
 	var widgetSrv
 	var mockPlease
@@ -18,17 +18,17 @@ describe('createCtrl', () => {
 		let app = angular.module('materia')
 		app.factory('Please', () => mockPlease)
 
-		require('../materia-namespace')
-		require('../materia-constants')
+		require('../common/materia-namespace')
+		require('../common/materia-constants')
 		require('../services/srv-datetime')
 		require('../services/srv-widget')
 		require('../services/srv-selectedwidget')
-		require('./ctrl-create')
+		require('./ctrl-widget-creator')
 
 		Object.defineProperty(window, 'location', {
 			value: {
-				href: url
-			}
+				href: url,
+			},
 		})
 
 		_widgetSrv = {}
@@ -38,10 +38,10 @@ describe('createCtrl', () => {
 
 		app.factory('dateTimeServ', () => _dateTimeServ)
 		app.factory('widgetSrv', () => _widgetSrv)
-		app.factory('userServ', () => _selectedWidgetSrv)
+		app.factory('SelectedWidgetSrv', () => _selectedWidgetSrv)
 		app.factory('Alert', () => _alert)
 
-		inject((_$controller_, _widgetSrv_, _selectedWidgetSrv_, _dateTimeServ_) => {
+		inject((_$controller_, _widgetSrv_, _dateTimeServ_) => {
 			$controller = _$controller_
 		})
 
@@ -49,23 +49,23 @@ describe('createCtrl', () => {
 	})
 
 	it('dispalys the rollback confirmation dialog after reloading', () => {
-		$controller('createCtrl', { $scope })
+		$controller('WidgetCreatorCtrl', { $scope })
 
 		var qset = JSON.stringify({
 			items: {
 				questions: [
 					{
-						text: 'aaa'
-					}
+						text: 'aaa',
+					},
 				],
 				answers: [
 					{
 						text: 'aaa',
-						value: '100'
-					}
+						value: '100',
+					},
 				],
-				options: {}
-			}
+				options: {},
+			},
 		})
 
 		expect($scope.showActionBar).toBe(true)
